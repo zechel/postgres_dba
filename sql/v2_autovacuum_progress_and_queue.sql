7603ca3e364d20370c8f31b2f33832e1a809b448
José Zechel <zechel@gmail.com>
2026-08-24 10:56:31 -0300

    feat(vacuum): improve autovacuum queue report
    Adapted from upstream commits 6d1cbd0 and 1e92365.

--- sql/v2_autovacuum_progress_and_queue.sql
+++ sql/v2_autovacuum_progress_and_queue.sql
@@ -40,14 +40,16 @@ select
   psat.n_dead_tup,
   'vt: ' || vacuum_settings.autovacuum_vacuum_threshold
     || ', vsf: ' || vacuum_settings.autovacuum_vacuum_scale_factor
-    || ', av: ' || case when not autovacuum_enabled then 'off' else 'on' end as "effective_settings",
+    || case
+      when not autovacuum_enabled then ', autovacuum: DISABLED'
+      else ', autovacuum: enabled'
+    end as "effective_settings",
   case
     when last_autovacuum > coalesce(last_vacuum, '0001-01-01') then left(last_autovacuum::text, 19) || ' (auto)'
     when last_vacuum is not null then left(last_vacuum::text, 19) || ' (manual)'
     else null
   end as "last_vacuumed",
-  p.phase as status,
-  -- coalesce(p.phase, '~~~ in queue ~~~') as status,
+  coalesce(p.phase, '~~~ in queue ~~~') as status,
   p.pid as pid,
   case
     when a.query ~ '^autovacuum.*to prevent wraparound' then 'wraparound'
@@ -55,7 +57,12 @@ select
     when a.pid is null then null
     else 'regular'
   end as mode,
-  case when a.pid is null then null else coalesce(wait_event_type ||'.'|| wait_event, 'f') end as waiting,
+  case
+    when a.pid is null then null
+    when wait_event_type is not null
+      then format('%s:%s', wait_event_type, wait_event)
+    else 'CPU*'
+  end as waiting,
   round(100.0 * p.heap_blks_scanned / nullif(p.heap_blks_total, 0), 1) AS scanned_pct,
   round(100.0 * p.heap_blks_vacuumed / nullif(p.heap_blks_total, 0), 1) AS vacuumed_pct,
   p.index_vacuum_count,
